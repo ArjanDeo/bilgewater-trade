dd63e99ad05b8d64a1febceecd1815fc3aa61ffa
Arjan Deo <arjandeo@bluecomet-it.com>
2026-08-17 08:17:45 +0100

    Fix int32 limit error with listing prices.

---
+++ src/BilgewaterTrade.DataAccess/Migrations/20260817071330_ChangePricesToLong.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BilgewaterTrade.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePricesToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "BuyoutCopper",
+                table: "RealmListings",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UnitPriceCopper",
+                table: "CommodityListings",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BuyoutCopper",
+                table: "RealmListings",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UnitPriceCopper",
+                table: "CommodityListings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
